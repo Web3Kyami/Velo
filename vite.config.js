b846722156efd78c925d79af136f046d53aa1899
Web3Kyami <web3kyami@gmail.com>
2026-09-11 18:51:47 +0100

    Fix Vercel build target for Markets redesign

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { createCallsApi } from './server/calls-api.js'
 
 export default defineConfig({
+  build: {
+    target: 'esnext',
+  },
   plugins: [{
     name: 'velo-api',
     configureServer(server) {
